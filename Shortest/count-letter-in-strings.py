d93518498cbe1d011433f5ac1b9835c67ecc5057
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-10 15:37:45 +0200

    Create count-letter-in-strings.py

---
+++ Shortest/count-letter-in-strings.py
@@ -0,0 +1,15 @@
+# Description:
+# Over the centuries a language evolves, and not only do words appear or disappear, but some letters become more used or on the contrary less used.
+# In order to be able to quickly analyze many texts, we want to develop a program that calculates how many times a given letter is present in a text.
+
+# Input:
+# Line 1: A character C that will be searched for
+# Line 2: An integer N
+# Next N lines: a string with length len
+
+# Output:
+# the number of times that C appears in the text
+
+i=input
+s=i()
+i(sum(i().count(s)for _ in'_'*int(i())))
