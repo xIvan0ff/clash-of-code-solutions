845c1e0cfcd8ef2f99101d411a58a23083d5739c
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-10 20:08:41 +0200

    Create sum-of-digits-binary.py

---
+++ Shortest/sum-of-digits-binary.py
@@ -0,0 +1,12 @@
+# Description
+# Print the sum of digits of a decimal number converted to binary.
+# Example: 15 in decimal => 1111 in binary, output will be 4.
+
+# Input
+# Input < 999999
+
+# Output
+# Output < 21
+
+# 42 CHARACTERS
+print(sum(map(int,bin(int(input()))[2:])))
