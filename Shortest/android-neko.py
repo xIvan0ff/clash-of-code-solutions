a883328bcf87469e0b085d4e253a7ceb9e777c96
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-10 15:20:28 +0200

    Create android-neko.py

---
+++ Shortest/android-neko.py
@@ -0,0 +1,28 @@
+# Description
+# Android Neko is a game developed by Android as an easter egg for Android 7 and 11. In Android 7, you play it like this:
+#
+# 1) Choose the food you want to put in the bowl
+# 2) Wait for a random cat to come
+# 3) Name it (If you want to)
+#
+# How long would it take to speedrun the game (in seconds) if you collect N cats if it takes:
+#
+# 1) P seconds to place food for 1 cat
+# 2) C + ((i-1)*2)) seconds to collect the ith cat
+# 3) R seconds to name a cat 
+
+# Input
+# Line 1: N
+# Line 2: P
+# Line 3: C
+# Line 4: R
+
+# Output
+# Time to collect N cats
+
+
+# 54 CHARACTERS
+
+f=lambda:int(input())
+n=f()
+input(n*(n+f()+f()+f()-1))
