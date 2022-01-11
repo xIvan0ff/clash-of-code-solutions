e73293e4e7d06165d79723b9ba282788f6c7acdc
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-11 22:44:24 +0200

    Create sum-of-digits-until-digit.py

---
+++ Shortest/sum-of-digits-until-digit.py
@@ -0,0 +1,16 @@
+# Description
+# A number N is received.
+
+# While N > 9:
+# Make the sum of each digit in the number.
+
+# Input
+# Line 1: N the number.
+
+# Output
+# A single digit <= 9
+
+# 35 CHARACTERS
+
+n=int(input())
+print(n%9or n and 9)
