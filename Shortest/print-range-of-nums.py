7360f6f4d2c4a8c512deef244f6f61548bb73f2e
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-11 16:14:39 +0200

    Update print-range-of-nums.py

--- Shortest/print-range-of-nums.py
+++ Shortest/print-range-of-nums.py
@@ -8,11 +8,11 @@
 # If n is negative, print numbers from n to 0 by adding 1 to n. If positive, print numbers from n-1 to 0 by subtracting 1 from n. If n=0 then print "Already Zero".
 
 
-# XXX Characters
+# 81 Characters
 
 I=input
 n=int(I())
 if n==0:I("Already Zero")
-if n>0:n-=1
-a=n<0 or-1
+a=1
+if n>0:n-=1;a-=2
 print(*range(n,a,a))
