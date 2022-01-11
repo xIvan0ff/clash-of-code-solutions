21d30f15078c45426876e26c8bd92fb73210578d
Hristiyan Ivanov <56456762+xIvan0ff@users.noreply.github.com>
2022-01-11 17:26:10 +0200

    Create distinct-number.py

---
+++ Shortest/distinct-number.py
@@ -0,0 +1,19 @@
+# Description
+# find the distinct number between 3 integers a, b and c
+# Note: There always two number are the same
+
+# Input
+# Line 1: An integer a
+# Line 2: An integer b
+# Line 3: An integer c
+
+# Output
+# Line 1 : A distinct number
+
+# 56 CHARACTERS
+
+I=input
+a,b,c=I(),I(),I()
+if a==b:I(c)
+if a==c:I(b)
+I(a)
